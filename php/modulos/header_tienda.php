75bd4d114364d43f4b909ec65f75e061750b1ea0
Xxdarknight0xX <114773358+Xxdarknight0xX@users.noreply.github.com>
2023-05-23 15:31:03 -0500

    cambios

--- php/modulos/header_tienda.php
+++ php/modulos/header_tienda.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script defer src="https://kit.fontawesome.com/49b40f8ce6.js" crossorigin="anonymous"></script>
     <script defer type="text/javascript" src="js/app.js"></script>
-    <title>Header</title>
+    <link rel="stylesheet" href="style_shop.css">
+    <title>Tienda</title>
 </head>
 <body>
 <body onscroll="Camb()">
@@ -16,13 +17,20 @@
             <nav class="nav" id="nav">
                 <button class="c-mu" id="cerrar"><i class="fa-solid fa-x"></i></button>
             <ul class="nav-list">
-                <li><a>Clases</a></li>
-                <li><a>Planes</a></li>
-                <li><a>Desafios</a></li>
-                <li><a>Contactos</a></li>
-                <li><a class="tienda" href="tienda.php">Tienda</a></li>
-                <li><button onclick="enviar_form()" class="bo"><p>Comience ya!</p></button></li>
+                <li><a>Inicio</a></li>
+                <li><a>Suplementos</a></li>
+                <li><a>Implementos</a></li>
+
+                <div class="form">
+  <input class="input" placeholder="Buscar" required="" type="text">
+  <span class="input-border"></span>
+</div>
+
+                <div class="icons">
                 <li><a class="b2" onclick=""><img src="img/login.svg" alt="" class="login"></a></li>
+                <li><a class="b3" onclick=""><img src="img/shopping-bag.png" alt="" class="login"></a></li>
+                </div>
+
             </ul>
         </nav>
     </header>
