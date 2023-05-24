996cfacbf17acaa68c759beb9a5bc56242fd1c0e
Xxdarknight0xX <114773358+Xxdarknight0xX@users.noreply.github.com>
2023-05-23 20:42:30 -0500

    carga de tienda
    se modifico el css para el buscador
    se puso un svg de una bolsa de compras
    se maqueto el buscador en html
    se modifico el header de la seccion de la tienda

--- php/modulos/header_tienda.php
+++ php/modulos/header_tienda.php
@@ -21,17 +21,25 @@
                 <li><a>Suplementos</a></li>
                 <li><a>Implementos</a></li>
 
-                <div class="form">
-  <input class="input" placeholder="Buscar" required="" type="text">
-  <span class="input-border"></span>
+                <div class="search">
+    <input type="text" class="search__input" placeholder="Buscar elemento">
+    <button class="search__button">
+        <svg class="search__icon" aria-hidden="true" viewBox="0 0 24 24">
+            <g>
+                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+            </g>
+        </svg>
+    </button>
 </div>
 
-                <div class="icons">
+<div class="icons">
+                 <li><a class="b3" onclick=""><img src="img/shop-bag.svg" alt="" class="login"></a></li>
                 <li><a class="b2" onclick=""><img src="img/login.svg" alt="" class="login"></a></li>
-                <li><a class="b3" onclick=""><img src="img/shopping-bag.png" alt="" class="login"></a></li>
+
                 </div>
 
             </ul>
+
         </nav>
     </header>
 </body>
