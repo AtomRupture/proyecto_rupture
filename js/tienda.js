20159b0071bf9aa61d8ff5babc8b807679f5ca52
Xxdarknight0xX <114773358+Xxdarknight0xX@users.noreply.github.com>
2023-05-23 21:59:20 -0500

    Update tienda.js

--- js/tienda.js
+++ js/tienda.js
@@ -1,12 +1,26 @@
 
-
 const slider = document.querySelector('.slider');
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
 let count = 0;
+let autoSlideInterval;
 
 function slide() {
   slider.style.transform = `translateX(-${count * 100}%)`;
+}
+
+function startAutoSlide() {
+  autoSlideInterval = setInterval(() => {
+    count++;
+    if (count > 2) {
+      count = 0;  // Ajusta este valor según la cantidad de imágenes que tengas
+    }
+    slide();
+  }, 5000);  // Ajusta este valor para controlar la velocidad del slider (en milisegundos)
+}
+
+function stopAutoSlide() {
+  clearInterval(autoSlideInterval);
 }
 
 prevButton.addEventListener('click', () => {
@@ -15,6 +29,7 @@ prevButton.addEventListener('click', () => {
     count = 2;  // Ajusta este valor según la cantidad de imágenes que tengas
   }
   slide();
+  stopAutoSlide();
 });
 
 nextButton.addEventListener('click', () => {
@@ -23,4 +38,7 @@ nextButton.addEventListener('click', () => {
     count = 0;  // Ajusta este valor según la cantidad de imágenes que tengas
   }
   slide();
+  stopAutoSlide();
 });
+
+startAutoSlide();
