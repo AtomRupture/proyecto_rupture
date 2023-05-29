85cc7dc856fd21b33d4571874d36045f2456fcd5
Xxdarknight0xX <114773358+Xxdarknight0xX@users.noreply.github.com>
2023-05-29 13:54:57 -0500

    organice algunas cosas de la pestaña tienda con el fin de verse mas estetico

--- tienda.php
+++ tienda.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style_shop.css">
     <script defer type="text/javascript" src="js/tienda.js"></script>
     <title>Tienda</title>
@@ -25,8 +26,21 @@
   </div>
 </div>
 
+<div class="block">
+
+<h1>Nuestros Productos</h1>
+
+</div>
+
 </section>
 
+
+<section class="sec2">
+
+
+
+
+</section>
 
     
 </body>
