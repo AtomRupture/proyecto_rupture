86a8d5b8eb561d17f991f56962da971b025f6f80
Xxdarknight0xX <114773358+Xxdarknight0xX@users.noreply.github.com>
2023-09-11 15:56:14 -0500

    arregle un poco el footer y ya se ve

--- php/modulos/footer_def.php
+++ php/modulos/footer_def.php
@@ -2,8 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/footer_def.css">
+    <link rel="stylesheet" href="responsive.css">
+
     <script defer type="text/javascript" src="./js/script.js"></script>
     <title>Document</title>
 </head>
@@ -11,140 +14,160 @@
 
 
 
-
-        <!-- footer area start -->
-        <div id="footer">
-            <!-- subscribe area start -->
-            <div class="subscribe-area">
-                <div class="section-wrap">
-                    <div class="subscribe-wrap">
-                        <div class="subscribe-text">
-                            <p>Subscribe to get all product updates first</p>
-                        </div>
-
-                        <form class="subscribe-input">
-                            <input type="email" placeholder="Enter your email" required>
-                            <button type="submit">Subscribe</button>
-                        </form>
+    <!-- footer section start -->
+    <div id="footer">
+        <!-- subscribe area start -->
+        <div class="subscribe-area">
+            <div class="section-wrap">
+                <div class="subscribe-wrap">
+                    <div class="subscribe-text">
+                        <p>Subscribe to get all product updates first</p>
                     </div>
+                    <form class="subscribe-input">
+                        <input type="email" placeholder="Enter your email" required>
+                        <button type="submit">Subscribe</button>
+                    </form>
                 </div>
             </div>
-            <!-- subscribe area end -->
-
-
-            <!-- footer navigation area start -->
-            <div class="section-footer">
-                <div class="section-wrap">
-                    <div class="footer-wrap">
-                        <div class="company-details">
-                            <h2>Fruty</h2>
-
-                            <div class="fdetails">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto deserunt et distinctio Animi possimus sed.</p>
-                            </div>
-
-                            <div class="social-media-links">
-                                <div class="f-links">
-                                    <a href="#"><span class="icon-facebook-f"></span></a>
-                                </div>
-
-                                <div class="f-links">
-                                    <a href="#"><span class="icon-instagram"></span></a>
-                                </div>
-
-                                <div class="f-links">
-                                    <a href="#"><span class="icon-linkedin-in"></span></a>
-                                </div>
-
-                                <div class="f-links">
-                                    <a href="#"><span class="icon-twitter"></span></a>
-                                </div>
-
-                                <div class="f-links">
-                                    <a href="#"><span class="icon-telegram-plane"></span></a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="footer-menu">
-                            <h2>Menu</h2>
-                            <div class="fmenu">
-                                <p><a href="#top">Home</a></p>
-                                <p><a href="#about">About</a></p>
-                                <p><a href="#services">Services</a></p>
-                                <p><a href="#products">Products</a></p>
-                                <p><a href="#contact">Contact</a></p>
-                            </div>
-                        </div>
-
-                        <div class="top-products-links">
-                            <h2>Top Products</h2>
-                            <div class="flinks">
-                                <p><a href="#products">Apple</a></p>
-                                <p><a href="#products">Grapes</a></p>
-                                <p><a href="#products">Mangos</a></p>
-                                <p><a href="#products">Oranges</a></p>
-                                <p><a href="#products">Pinapple</a></p>
-                            </div>
-                        </div>
-
-                        <div class="useful-links">
-                            <h2>Quick Links</h2>
-                            <div class="Qlinks">
-                                <p><a href="#">User Account</a></p>
-                                <p><a href="#">Become An Affilate</a></p>
-                                <p><a href="#">New Offer</a></p>
-                                <p><a href="#">Recent Blogs</a></p>
-                                <p><a href="#">Help</a></p>
-                            </div>
-                        </div>
-
-                        <div class="master-cards">
-                            <h2>Easy Payment</h2>
-                            <div class="payment-cards">
-                                <div class="payment-link">
-                                    <a href="#"><span class="icon-cc-amazon-pay"></span></a>
-                                </div>
-
-                                <div class="payment-link">
-                                    <a href="#"><span class="icon-cc-mastercard"></span></a>
-                                </div>
-
-                                <div class="payment-link">
-                                    <a href="#"><span class="icon-cc-paypal"></span></a>
-                                </div>
-
-                                <div class="payment-link">
-                                    <a href="#"><span class="icon-cc-stripe"></span></a>
-                                </div>
-
-                                <div class="payment-link">
-                                    <a href="#"><span class="icon-cc-visa"></span></a>
-                                </div>
-
-                                <div class="payment-link">
-                                    <a href="#"><span class="icon-cc-discover"></span></a>
-                                </div>
-
-                                <div class="payment-link">
-                                    <a href="#"><span class="icon-cc-jcb"></span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- footer navigation area end -->
-
-
-            <!-- copyright area start -->
-            <footer>
-                <p>Created By | &copy; 2021 All rights reserved</p>
-            </footer>
-            <!-- copyright area end -->
-
         </div>
-        <!-- footer area end -->
+        <!-- subscribe area end -->
+
+
+
+        <!-- footer navigation area start -->
+        <div class="section-footer">
+            <div class="section-wrap">
+                <div class="footer-wrap">
+                    <div class="company-details">
+                        <h2>Fruty</h2>
+
+                        <div class="fdetails">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto deserunt et
+                                distinctio Animi
+                                possimus sed.</p>
+                        </div>
+
+                        <div class="social-media-links">
+                            <div class="f-links">
+                                <a href="#"><span class="icon-facebook-f"></span></a>
+                            </div>
+
+                            <div class="f-links">
+                                <a href="#"><span class="icon-instagram"></span></a>
+                            </div>
+
+                            <div class="f-links">
+                                <a href="#"><span class="icon-linkedin-in"></span></a>
+                            </div>
+
+                            <div class="f-links">
+                                <a href="#"><span class="icon-twitter"></span></a>
+                            </div>
+
+                            <div class="f-links">
+                                <a href="#"><span class="icon-telegram-plane"></span></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="footer-menu">
+                        <h2>Menu</h2>
+                        <div class="fmenu">
+                            <p><a href="#top">Home</a></p>
+                            <p><a href="#about">About</a></p>
+                            <p><a href="#services">Services</a></p>
+                            <p><a href="#products">Products</a></p>
+                            <p><a href="#contact">Contact</a></p>
+                        </div>
+                    </div>
+
+                    <div class="top-products-links">
+                        <h2>Top Products</h2>
+                        <div class="flinks">
+                            <p><a href="#products">Apple</a></p>
+                            <p><a href="#products">Grapes</a></p>
+                            <p><a href="#products">Mangos</a></p>
+                            <p><a href="#products">Oranges</a></p>
+                            <p><a href="#products">Pinapple</a></p>
+                        </div>
+                    </div>
+
+                    <div class="useful-links">
+                        <h2>Quick Links</h2>
+                        <div class="Qlinks">
+                            <p><a href="#">User Account</a></p>
+                            <p><a href="#">Become An Affilate</a></p>
+                            <p><a href="#">New Offer</a></p>
+                            <p><a href="#">Recent Blogs</a></p>
+                            <p><a href="#">Help</a></p>
+                        </div>
+                    </div>
+
+                    <div class="master-cards">
+                        <h2>Easy Payment</h2>
+                        <div class="payment-cards">
+                            <div class="payment-link">
+                                <a href="#"><span class="icon-cc-amazon-pay"></span></a>
+                            </div>
+
+                            <div class="payment-link">
+                                <a href="#"><span class="icon-cc-mastercard"></span></a>
+                            </div>
+
+                            <div class="payment-link">
+                                <a href="#"><span class="icon-cc-paypal"></span></a>
+                            </div>
+
+                            <div class="payment-link">
+                                <a href="#"><span class="icon-cc-stripe"></span></a>
+                            </div>
+
+                            <div class="payment-link">
+                                <a href="#"><span class="icon-cc-visa"></span></a>
+                            </div>
+
+                            <div class="payment-link">
+                                <a href="#"><span class="icon-cc-discover"></span></a>
+                            </div>
+
+                            <div class="payment-link">
+                                <a href="#"><span class="icon-cc-jcb"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- footer navigation area end -->
+
+
+
+        <!-- copyright area start -->
+        <footer>
+            <p>Created By <a href="https://www.youtube.com/channel/UCr4TC9YxsDZwzwIxnZOVlBw" target="_blank">Front End
+                    Library</a> |
+                &copy; 2021 All rights reserved</p>
+        </footer>
+        <!-- copyright area end -->
+    </div>
+    <!-- footer section end -->
+
+
+
+    <!-- owl carousel cdn -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+        integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- external plugins script -->
+    <script src="js/library.js"></script>
+
+    <!-- custom script -->
+    <script src="js/script.js"></script>
+
     
 </body>
 </html>
