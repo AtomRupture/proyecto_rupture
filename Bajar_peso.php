02b46ce70ae0050e31f5def054809df2b5b9e38c
Xxdarknight0xX <114773358+Xxdarknight0xX@users.noreply.github.com>
2023-09-15 17:42:29 -0500

    nuevas secciones

---
+++ Bajar_peso.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style_bajar.css">
+    <script src="js/dietas.js"></script>
+	
+    
+    <title>Bajar Peso</title>
+</head>
+<?php include './php/modulos/header.php' ?>
+<body>
+ 
+<section class="secti1"></section>
+
+<div id="overlay">
+    <div id="title"><h1>Parallaxelicious</h1></div>
+    <video class="visible-desktop" id="hero-vid" poster="./video/video_bajarp.mp4" autoplay loop muted>
+        <source type="video/webm" src="./video/video_bajarp.mp4"></source>
+        <source type="video/mp4" src="./video/video_bajarp.mp4"></source>
+    </video>
+    <div id="state" class="visible-desktop"><span class="fa fa-pause"></span></div>
+    <img id="hero-pic" class="hidden-desktop" src="https://www.markhillard.com/sandbox/media/polina.jpg" alt="">
+</div>
+
+
+</body>
+
+<?php include './php/modulos/footer_def.php'?>
+
+</html>
