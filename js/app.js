c66f7d00d6971485cc6659db73fba9a8314135c3
AtomRupture <atomrupture@gmail.com>
2023-09-15 17:06:41 -0500

    Header universal
    Desde ahora el header es universal a todas las paginas ademas de que ahora es mas facil de usar esto junto con unos pocos arreglos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 "use strict";
-// alert("header test");
+alert("header test");
 
 
 const enviar_form = () =>{
--- php/modulos/header.php
+++ php/modulos/header.php
@@ -5,9 +5,243 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script defer src="https://kit.fontawesome.com/49b40f8ce6.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <script src="\proyecto_rupture\js\app.js"></script>
-    <script defer type="text/javascript" src="js/app.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />    
+    <style>
+        
+    header{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: rgba(0, 0, 0, 0.4);
+        width: 100%;
+        height: 5.5rem;
+        position: fixed;
+        z-index: 9;
+        color: white;
+    }
+
+
+    .nav-list{
+        display: flex;
+        list-style-type: none;
+        gap: 2.5rem;
+        font-weight: 700;
+        color: white;
+        padding-top: 2rem;
+        padding-left: 1rem;
+        font-size: 15px;
+        float: right;
+    }
+
+    .sub-menu-warp {
+    position: absolute;
+    top: 5.4rem;
+    right: -13rem;
+    width: 25rem;
+    max-height: 41rem;
+    overflow: hidden;
+    transition: max-height .5s;
+    display: none;
+    }
+
+    .sub-menu-warp.open-menu {
+    display: flex; 
+    }
+
+
+
+    .sub-menu{
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 0.5rem;
+    margin: 0.1rem;
+    height: 90vh;
+    width: 45%;
+    }
+
+    .sub-menu hr{
+    border: 0;
+    height: 1px;
+    width: 100%;
+    background: #ccc;
+    margin: 15px 0 10px; 
+    }
+
+
+    .user-info h3{
+    font-weight: 500;
+    }
+
+    .user-info{
+    display: flex;
+    align-items: center;
+    width: 100px;
+    border-radius: 50%;
+    margin-right: 15px;
+    margin-left: -2rem;
+    }
+
+    .sub-menu-link{
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    margin: 8px 80px 70px;
+    flex-direction: column;
+    /* align-content: center; */
+    justify-content: flex-end;
+    }
+
+    .sub-menu-link p {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    }
+
+    .sub-menu h2{
+    margin-top: 6rem;
+    margin-left: -5rem;
+    transition: .5s;
+    }
+
+    .sub-menu-link:hover span{
+    transform: scale(1.3);
+    }
+
+    .sub-menu-link:hover p{
+    font-weight: 600 !important;
+    }
+
+
+
+    .nav-list li button{
+        text-decoration: none;
+        color: white;
+        cursor: pointer;
+    }
+
+    .material-symbols-outlined {
+    font-size: 4rem !important;
+    }
+    .nav-list button{
+
+        font-style: italic;
+        background-color: #E40b0b;
+        border: transparent;
+        border-radius: 50px;
+        width: 150px;
+        height: 42px;
+        font-weight: 700;
+        font-size: 19px;
+        color: white;
+        cursor: pointer;
+        margin-top: -0.5rem;
+        transition: .3s;
+        
+    }
+    button p{
+        margin-top: 0.4rem;
+    }
+    .nav-list button:hover{
+        background-color: black;
+        border: #E40b0b solid;
+        transition: border ease-in-out 250ms;
+    }
+
+    .nav-list a{
+        color: white !important;
+        text-decoration: none !important;
+        cursor: pointer;
+        padding-bottom: 0.2rem;
+        border: #E40b0b 0px;
+    }
+
+    .nav-list a:hover{
+        border: #E40b0b solid 4px;
+        border-radius: 3px;
+        transition: border ease-in-out 120ms;
+        border-top: transparent;
+        border-left: transparent;
+        border-right: transparent;
+        
+    }
+
+    .b2{
+        font-style: italic;
+        background-color: transparent !important;
+        font-weight: 700;
+        font-size: 19px;
+        color: white;
+        cursor: pointer;
+        max-width: 1rem;
+        margin-top: -1rem;
+        padding-right: 3rem;
+        transition: .4s;
+    }
+
+    .nav-list .b2:hover{
+        border: none;
+        
+    }
+
+    .logo{
+        max-width: 8rem;
+        padding-left: 2.4rem;
+    }
+
+    .login{
+        font-style: italic;
+        background-color: transparent !important;
+        font-weight: 700;
+        font-size: 19px;
+        color: white;
+        cursor: pointer;
+        max-width: 5rem;
+        margin-top: -1rem;
+        margin-left: 1.0rem;
+    }
+
+    .b2:hover,
+    .login:hover {
+    cursor: pointer;
+    filter: brightness(0.7);
+    fill: #E40b0b;
+    }
+
+    .a-mus{
+        display: block;
+        background-color: transparent;
+        color: white;
+        border: none !important;
+        width: 5rem;
+        cursor: pointer;
+        font-size: 2rem;
+    }
+
+    .c-mus{
+        display: none;
+        background-color: transparent;
+        color: #E40b0b;
+        border: none !important;
+        width: 5rem;
+        cursor: pointer;
+        font-size: 2rem;
+        padding-left: 1rem;
+        padding-top: 0rem;
+    }
+    .contenedor{
+        margin:8rem auto;
+    }
+    .b2{
+        position: relative;
+        z-index: 2;
+    }
+    
+    #op{
+        position: relative;
+        z-index: 2;
+    }
+
+    </style>
+    <script defe src="\proyecto_rupture\js\app.js"></script>
     <title>Header</title>
 </head>
 <!-- <?php 
@@ -18,7 +252,7 @@
 <body>
 <body onscroll="Camb()">
     <header id="headt"> 
-           <a href="index.php"> <img src="img/unnamed-removebg-preview.svg" alt="logo" class="logo"></a>
+           <a href="\proyecto_rupture\index.php"> <img src="img/unnamed-removebg-preview.svg" alt="logo" class="logo"></a>
             <button class="a-mu" id="abrir"><i class="fa-solid fa-bars"></i></button>
             <nav class="nav" id="nav">
                 <button class="c-mu" id="cerrar"><i class="fa-solid fa-x"></i></button>
@@ -66,4 +300,3 @@
     </header>
 </body>
 </html>
-
