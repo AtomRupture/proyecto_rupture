690f0d7b8001f14be90775a75ed7dbce1e4bb1fb
JhonP16 <jhonpulgarinrestrepo@gmail.com>
2023-09-21 17:24:00 -0500

    Creada landing para las clases

---
+++ landing_clases.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es-co">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/49b40f8ce6.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="shortcut icon" href="./img/unnamed-removebg-preview.svg">
+    <link rel="stylesheet" href="css/style_landing_rutinas.css">
+    <title>Clases</title>
+</head>
+<body>
+
+<?php include './php/modulos/header.php' ?>
+
+<section class="showcase">
+			<div class="video-container">
+				<video src="video/landing_clases.mp4" autoplay muted loop></video>
+			</div>
+			<div class="content">
+				<h1>Clases Atom</h1>
+				<h4>Es tu momento</h4>
+                <div class="bt">
+                <button class="emp-ahr" onclick="alerta();">
+                    REGISTRATE
+                </button>
+            </div>
+			</div>
+</section>
+
+        <script>
+        function alerta(){
+            Swal.fire({
+                title: '<strong>Registrate</strong>',
+                icon: 'info',
+                html:
+                    'Para acceder debes <br> <b> registrarte</b>',
+                showCloseButton: true,
+                showCancelButton: true,
+                focusConfirm: false,
+                confirmButtonText:
+                    '<a href="./login.php"><i class="fa fa-thumbs-up"></i> Vamos!</a>',
+                confirmButtonAriaLabel: 'Thumbs up, great!',
+                cancelButtonText:
+                    '<i class="fa fa-thumbs-down"></i>',
+                cancelButtonAriaLabel: 'Thumbs down'
+                });
+        }
+    </script>
+    
+</body>
+</html>
---
+++ php/user/user_landclases.php
@@ -0,0 +1,68 @@
+<?php
+
+    session_start();
+    error_reporting(0);
+
+    $varsesion = $_SESSION['nombre'];
+
+    if($varsesion == null || $varsesion = ''){
+        echo "No tiene permiso para ingresar";
+        die();
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="es-co">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/49b40f8ce6.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="shortcut icon" href="../../img/unnamed-removebg-preview.svg">
+    <link rel="stylesheet" href="../../css/style_landing_rutinas.css">
+    <title>Rutinas</title>
+</head>
+<body>
+
+<?php include '../modulos/header-af.php' ?>
+
+<section class="showcase">
+			<div class="video-container">
+				<video src="../../video/landing_clases.mp4" autoplay muted loop></video>
+			</div>
+			<div class="content">
+				<h1>Clases Atom</h1>
+				<h4>Es tu momento</h4>
+                <div class="bt">
+                <button class="emp-ahr" onclick="mostrar();">
+                    EMPIEZA AHORA
+                </button>
+            </div>
+			</div>
+		</section>
+
+        
+        <script>
+        function mostrar(){
+        Swal.fire({
+        title: 'Haz parte!',
+        text: 'Empieza el cambio con nuestras rutinas',
+        imageUrl: '../../img/img_clases/project-1.png',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+                showCancelButton: true,
+        confirmButtonText:
+                    '<a href="user_clases.php"><i class="fa fa-thumbs-up"></i> Vamos!</a>',
+                confirmButtonAriaLabel: 'Thumbs up, great!'
+        })
+        }
+    </script>
+    
+</body>
+</html>
