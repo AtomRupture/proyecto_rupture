85e487c1abbf2ac618807b73a3a29b5d7bef9f5e
JhonP16 <jhonpulgarinrestrepo@gmail.com>
2023-10-16 18:34:21 -0500

    Finalizada rutina antebrazo

---
+++ php/user/programa_rutinas/js_programas/insertarBrazoComp.js
@@ -0,0 +1,31 @@
+let gifs = [
+    {
+        numb: 1, //INDEX 0
+        question: "Curl de bíceps invertido",
+        gif: "<img width='350' src='img_programas/curl_invertido.gif'"
+    },
+
+    {
+        numb: 2, //INDEX 1
+        question: "Martillo",
+        gif: "<img width='350' src='img_programas/martillo.gif'"
+    },
+
+    {
+        numb: 3, //INDEX 2
+        question: "Copa",
+        gif: "<img width='350' src='img_programas/copa.gif'"
+    },
+
+    {
+        numb: 4, //INDEX 2
+        question: "Francés",
+        gif: "<img width='350' src='img_programas/frances.gif'"
+    },
+
+    {
+        numb: 5, //INDEX 2
+        question: "Flexión de muñeca",
+        gif: "<img width='350' src='img_programas/flexion_muñeca.gif'"
+    },
+];
